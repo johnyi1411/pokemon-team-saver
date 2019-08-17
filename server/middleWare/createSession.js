bb919b13d5d2b7702bc139ce7e4cd97bbfd94d80
Bryan Liang <bryanliang1995@gmail.com>
2019-08-17 14:59:00 -0700

    added middle wares

--- server/middleWare/createSession.js
+++ server/middleWare/createSession.js
@@ -1,5 +1,12 @@
 const utils = require('../lib/hashUtils.js');
 
 const creatSession = (req, res, next) => {
-  
+  //if user has a cookie
+  if(Object.keys(req.cookies)) {
+
+  }
+  //user has no cookie
+  else {
+    
+  }
 }
--- server/server.js
+++ server/server.js
@@ -3,7 +3,10 @@ const app = express();
 const port = 3000;
 const path = require('path');
 const db = require('./../database/databaseHelpers.js');
+const cookieParser = require('cookie-parser');
 
+app.use(require('morgan')('dev'));
+app.use(cookieParser())
 app.use(express.static(__dirname + '/../public'))
 
 app.get('/createUser', (req, res, next) => {
@@ -12,7 +15,13 @@ app.get('/createUser', (req, res, next) => {
     res.end('hit');
 })
 
-app.listen(port, () => {
+app.get('/testCookies', (req, res, next) => {
+    console.log('cookies: ', req.cookies);
+    console.log('length: ', Object.keys(req.cookies).length);
+    res.cookie('testCookie', 123456789);
+    res.end();
+})
 
+app.listen(port, () => {
     console.log("Listening on port:", port);
 });
